2f4388a6f571e75a14b8a362ef874e832754b96f
DESKTOP-IQAIMOV\chris <Chris322P@googlemail.com>
2018-05-21 23:33:56 +0200

    additons to Inventory

--- Source/Clicker2/GameModeChris.h
+++ Source/Clicker2/GameModeChris.h
@@ -4,24 +4,69 @@
 
 #include "CoreMinimal.h"
 #include "includes/InfInt.h"
+#include "Engine/DataTable.h"
 #include "GameFramework/GameModeBase.h"
 #include "GameModeChris.generated.h"
 
-/**
- * 
- */
+
+USTRUCT(BlueprintType)
+struct FInventoryItem : public FTableRowBase
+{
+
+	GENERATED_BODY()
+
+public:
+
+	FInventoryItem()
+	{
+		Name = FText::FromString("Item");
+		Value = 10;
+		Description = FText::FromString("Item");
+	}
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FName ItemID;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FText Name;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int slot;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int Value;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UTexture2D* Thumbnail;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FText Description;
+
+	bool operator==(const FInventoryItem& Item) const
+	{
+		if (ItemID == Item.ItemID)
+			return true;
+		else
+			return false;
+	}
+
+};
+
+
 UCLASS()
 class CLICKER2_API AGameModeChris : public AGameModeBase
 {
 	GENERATED_BODY()
 	
+public:
+	UFUNCTION(BlueprintCallable)
+	class UDataTable* GetItemDB() const { return ItemDB; }
+
 private:
 	InfInt money;
 
-
-//	UPROPERTY(EditAnywhere)
-//		FString moneyString;
-	
+	UPROPERTY(EditDefaultsOnly)
+		class UDataTable* ItemDB;
 
 	UFUNCTION(BlueprintCallable)
 		void AddMoney(FString _money);
